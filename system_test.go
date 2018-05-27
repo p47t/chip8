4e9a593162b0caff272c555ba14fa3fae2610831
Patrick Tsai <patrick@simplypatrick.com>
2018-05-27 00:05:58 -0700

    Add benchmark of CPU interpreter

--- system_test.go
+++ system_test.go
@@ -1,1 +1,24 @@
 package chip8
+
+import "testing"
+
+func BenchmarkInvaders(b *testing.B) {
+	benchmarkRom(b, "roms/invaders.c8", 10000)
+}
+
+func BenchmarkPong2(b *testing.B) {
+	benchmarkRom(b, "roms/pong2.c8", 10000)
+}
+
+func benchmarkRom(b *testing.B, rom string, cycles int) {
+	var sys System
+	sys.Initialize()
+	sys.Load(rom)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		for i := 0; i < cycles; i++ {
+			sys.Cycle()
+		}
+	}
+}
